c8f4ec97cc577fc80b08c74b9d68a605723aab8e
Gilly7CE <gilly7ce@gmail.com>
2023-04-25 12:53:15 +0100

    Refactor PhantomSpawner_PhantomsObeyHostileMobCapMixin to work per player
    Introduced a new mixin: SpawnHelperInfoInvokerMixin
     - used to access the isBelowCap private method in SpawnHelper.Info
    
    PhantomSpawner_PhantomsObeyHostileMobCapMixin has been refactored to
    use this method, which reduces code duplication and will work per player.
    The target method is now to redirect the isSpectator method of a player,
    so when all world players are accessed in the phantomSpawner class, the
    ability to spawn will be determined by whether the per player cap is full
    or not.

--- src/main/java/gillycarpetaddons/mixins/PhantomSpawner_PhantomsObeyHostileMobCapMixin.java
+++ src/main/java/gillycarpetaddons/mixins/PhantomSpawner_PhantomsObeyHostileMobCapMixin.java
@@ -2,50 +2,54 @@ package gillycarpetaddons.mixins;
 
 import gillycarpetaddons.GillyCarpetAddonsSettings;
 import gillycarpetaddons.helpers.ChunkManagerHelper;
+import gillycarpetaddons.mixins.invokers.SpawnHelperInfoInvokerMixin;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.spawner.PhantomSpawner;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
- * This is adapted from the
+ * This is originally adapted from the
  * <a href="https://github.com/Lunaar-SMP/lunaar-carpet-addons">lunaar carpet extensions mod</a>.
  * At time of writing this code, the lunaar mod is still in version 1.17 and 1.18 experimental snapshots and I really
  * wanted this functionality for 1.19+. If this mod will support versions below 1.19, then only 1.18 will include this
  * carpet rule out of respect for the lunaar mod.
+ *
+ * Most of the code here is different to lunaar now, as the mob cap is per player and not global anymore.
  */
 @Mixin(PhantomSpawner.class)
-public class PhantomSpawner_PhantomsObeyHostileMobCapMixin {
-    @Inject(
+public abstract class PhantomSpawner_PhantomsObeyHostileMobCapMixin {
+    /**
+     * Redirects the isSpectator method of the playerEntity when called
+     * in the 'phantomSpawner.spawn' method. This is called in a loop of all
+     * world player entities, and we only want to spawn phantoms around players
+     * whose hostile mob cap is below the required values. This allows us to
+     * apply additional checks instead of just whether the player is in spectator.
+     * I did not know any other way of implementing this additional conditional!
+     * @param instance the player instance to check
+     * @return true if the player is in spectator mode or the player hostile mob cap is reached.
+     */
+    @Redirect(
             method = "spawn",
             at = @At(
-                    value = "FIELD",
-                    target = "Lnet/minecraft/world/spawner/PhantomSpawner;cooldown:I",
-                    opcode = 0xB5, // PUTFIELD
-                    ordinal = 1,
-                    shift = At.Shift.AFTER
-            ),
-            cancellable = true
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/player/PlayerEntity;isSpectator()Z"
+            )
     )
-    public void mobcapConditional(ServerWorld world,
-                                  boolean spawnMonsters,
-                                  boolean spawnAnimals,
-                                  CallbackInfoReturnable<Integer> cir) {
+    private boolean isSpectatorOrAtMobCap(PlayerEntity instance) {
+        boolean isSpectator = instance.isSpectator();
         if (!GillyCarpetAddonsSettings.phantomsObeyHostileMobCap) {
-            return;
+            return isSpectator;
         }
 
+        ChunkPos playerChunkPos = instance.getChunkPos();
         SpawnHelper.Info info = ChunkManagerHelper.getInfo();
-        int chunkArea = (int) Math.pow(17.0D, 2.0D);
-        int mobCap = SpawnGroup.MONSTER.getCapacity()
-                * ChunkManagerHelper.getSpawningChunkCount() / chunkArea;
-        int numberOfHostileMobs = info.getGroupToCount().getInt(SpawnGroup.MONSTER);
-        if (numberOfHostileMobs >= mobCap) {
-            cir.setReturnValue(0);
-        }
+        boolean canSpawn = ((SpawnHelperInfoInvokerMixin) info).invokeIsBelowCap(SpawnGroup.MONSTER, playerChunkPos);
+        return isSpectator | !canSpawn;
     }
 }
---
+++ src/main/java/gillycarpetaddons/mixins/invokers/SpawnHelperInfoInvokerMixin.java
@@ -0,0 +1,13 @@
+package gillycarpetaddons.mixins.invokers;
+
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.SpawnHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(SpawnHelper.Info.class)
+public interface SpawnHelperInfoInvokerMixin {
+    @Invoker("isBelowCap")
+    boolean invokeIsBelowCap(SpawnGroup group, ChunkPos chunkPos);
+}
