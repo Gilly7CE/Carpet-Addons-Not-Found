82ff462c2146282ae2384def45178633c0dc1df7
Gilly7CE <gilly7ce@gmail.com>
2023-04-26 11:49:56 +0100

    Refactor PistonBlock_MovableEndPortalFrameMixin to not allow movable full end portal frames

--- src/main/java/gillycarpetaddons/mixins/PistonBlock_MovableEndPortalFrameMixin.java
+++ src/main/java/gillycarpetaddons/mixins/PistonBlock_MovableEndPortalFrameMixin.java
@@ -1,10 +1,7 @@
 package gillycarpetaddons.mixins;
 
 import gillycarpetaddons.GillyCarpetAddonsSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.PistonBlock;
+import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,7 +18,10 @@ public class PistonBlock_MovableEndPortalFrameMixin {
             ))
     private static float getHardnessRedirect(BlockState instance, BlockView blockView, BlockPos blockPos) {
         Block currentBlock = instance.getBlock();
-        if (GillyCarpetAddonsSettings.movableEndPortalFrames && currentBlock == Blocks.END_PORTAL_FRAME) {
+        // Only allow empty end portal frames to be moved
+        if (GillyCarpetAddonsSettings.movableEndPortalFrames
+                && currentBlock == Blocks.END_PORTAL_FRAME
+                && !instance.get(EndPortalFrameBlock.EYE)) {
             // If hardness is -1.0f then the piston cannot be moved. This is the vanilla behaviour for end portal frames
             // which matches with the bedrock block. Instead of overriding the hardness for end portal frames everywhere,
             // just do it when it is called inside the PistonBlock.
