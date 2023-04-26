f15fe36b7002eccaacb99397ff56c0e61ab5559a
Gilly7CE <gilly7ce@gmail.com>
2023-04-26 17:29:53 +0100

    Extend EndPortalFrameHelper with logic from EndFrameBlock_EyeToggleMixin
    EndFrameBlock_EyeToggleMixin has had the removing portal and setting end
    portal state logic extracted into EndPortalFrameHelper.

--- src/main/java/gillycarpetaddons/helpers/EndPortalFrameHelper.java
+++ src/main/java/gillycarpetaddons/helpers/EndPortalFrameHelper.java
@@ -1,10 +1,50 @@
 package gillycarpetaddons.helpers;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.EndPortalFrameBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class EndPortalFrameHelper {
     public static boolean HasEyeOfEnder(BlockState blockState) {
         return blockState.get(EndPortalFrameBlock.EYE);
+    }
+
+    /**
+     * Sets an end portal frame blocks state to empty and breaks any connecting portals
+     * @param world the current world
+     * @param blockState the current block state
+     * @param pos the current block position
+     * @author serz
+     */
+    public static void setEmptyEndPortalFrameState(World world, BlockState blockState, BlockPos pos) {
+        BlockState newState = blockState.with(EndPortalFrameBlock.EYE, false);
+        world.setBlockState(pos, newState);
+        //remove portal this frame used to create
+        removePortal(world, pos.north());
+        removePortal(world, pos.west());
+        removePortal(world, pos.south());
+        removePortal(world, pos.east());
+    }
+
+
+    /**
+     * Recursive method to remove connecting portal blocks, if current block is
+     * an end portal.
+     * @param world the current world
+     * @param pos the current block position
+     * @author serz
+     */
+    private static void removePortal(World world, BlockPos pos){
+        if(world.getBlockState(pos).getBlock() != Blocks.END_PORTAL) {
+            return;
+        }
+
+        world.setBlockState(pos,Blocks.AIR.getDefaultState());
+        removePortal(world, pos.north());
+        removePortal(world, pos.west());
+        removePortal(world, pos.south());
+        removePortal(world, pos.east());
     }
 }
