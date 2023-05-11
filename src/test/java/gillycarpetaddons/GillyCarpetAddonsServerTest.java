3dacbd05a29b0b7b06daca1d5fb8c1be803c5936
Gilly7CE <gilly7ce@gmail.com>
2023-05-11 16:48:50 +0100

    Add GillyCarpetAddonsServerTest
    Test for the GillyCarpetAddonsServer class which will be loaded on
    starting the mod. Check is to ensure that nothing is missed in PRs or
    in local development.

---
+++ src/test/java/gillycarpetaddons/GillyCarpetAddonsServerTest.java
@@ -0,0 +1,30 @@
+package gillycarpetaddons;
+
+import carpet.CarpetServer;
+import net.minecraft.Bootstrap;
+import net.minecraft.SharedConstants;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for the GillyCarpetAddonsSetting class
+ */
+public final class GillyCarpetAddonsServerTest {
+    @BeforeAll
+    public static void setup() {
+        SharedConstants.createGameVersion();
+        Bootstrap.initialize();
+    }
+
+    /**
+     * Tests that the onGameStarted method runs without throwing an exception.
+     * This ensures the settings have been parsed okay.
+     * We call the method in CarpetServer to ensure everything is set up okay
+     */
+    @Test
+    public void onGameStartedTest() {
+        CarpetServer.manageExtension(new GillyCarpetAddonsServer());
+        Assertions.assertDoesNotThrow(CarpetServer::onGameStarted);
+    }
+}
