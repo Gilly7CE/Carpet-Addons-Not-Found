973f048e1b933567e627d6f7a1aceb06b5c6ebba
Serzbiedry <131688331+Serzbiedry@users.noreply.github.com>
2023-05-20 22:08:35 +0200

    tear of my skin over gillies requests
    Signed-off-by: serz <serekpl25@gmail.com>

---
+++ src/main/java/gillycarpetaddons/mixins/TallFlowerBlock_ReplaceFlowersMixin.java
@@ -0,0 +1,40 @@
+package gillycarpetaddons.mixins;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
+import net.minecraft.block.TallFlowerBlock;
+import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+import static gillycarpetaddons.GillyCarpetAddonsSettings.ReplaceableFlowersOptions;
+import static gillycarpetaddons.GillyCarpetAddonsSettings.replaceableFlowers;
+
+@Mixin(TallFlowerBlock.class)
+public abstract class TallFlowerBlock_ReplaceFlowersMixin extends Block {
+  TallFlowerBlock_ReplaceFlowersMixin() {
+    super(Settings.of(Material.PLANT));
+  }
+
+  @Override
+  public boolean canReplace(BlockState state, ItemPlacementContext context) {
+    if (replaceableFlowers != ReplaceableFlowersOptions.ALL_FLOWERS) {
+      //from TallFlowerBlock.canReplace
+      return false;
+    }
+    //From AbstrackBlock.canReplace but without Material.isReplacable check.
+    return context.getStack().isEmpty() || !context.getStack().isOf(this.asItem());
+  }
+
+  @Override
+  public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+    if (state.get(TallFlowerBlock.HALF) == DoubleBlockHalf.LOWER && newState.getMaterial() != Material.AIR) {
+      dropStack(world, pos, new ItemStack(this.asItem(), 1));
+    }
+    super.onStateReplaced(state, world, pos, newState, moved);
+  }
+}
