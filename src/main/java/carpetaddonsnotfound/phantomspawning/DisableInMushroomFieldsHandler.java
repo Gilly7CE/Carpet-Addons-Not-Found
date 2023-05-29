324dd123b47fa38840b2beb10d0fc432371e9a4b
Gilly7CE <gilly7ce@gmail.com>
2023-05-29 10:13:19 +0100

    Add phantomspawning package to carpetaddonsnotfound
    New package which is concerned with phantom spawning in the game. We
    have several rules pertaining to phantom spawning in the mod and this
    package aims to decouple each rule implementation from one another. This
    also aims to fix incompatability issues with other mods as most seem to
    target the same method with a redirect in PhantomSpawner. Handlers are
    introduced in this package which allow chaining of a "can a phantom
    spawn" request. This will make the mixin extremely lightweight and easier
    to adapt in the future if other incompatability issues are found.

---
+++ src/main/java/carpetaddonsnotfound/phantomspawning/DisableInMushroomFieldsHandler.java
@@ -0,0 +1,55 @@
+package carpetaddonsnotfound.phantomspawning;
+
+import carpetaddonsnotfound.CarpetAddonsNotFoundSettings;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
+
+/**
+ * The phantom spawning handler for the
+ * {@link carpetaddonsnotfound.CarpetAddonsNotFoundSettings#disablePhantomSpawningInMushroomFields} rule.
+ *
+ * @author Gilly7CE
+ * @see PhantomSpawningHandler
+ */
+public final class DisableInMushroomFieldsHandler implements PhantomSpawningHandler {
+  private PhantomSpawningHandler nextHandler = new DefaultHandler();
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public PhantomSpawningHandler setNextHandler(PhantomSpawningHandler nextHandler) {
+    this.nextHandler = nextHandler;
+    return this.nextHandler;
+  }
+
+  /**
+   * Determines whether a phantom can spawn around the player
+   *
+   * @param playerEntity
+   *         the player entity to conditionally spawn the phantom around
+   * @param world
+   *         the current minecraft world.
+   *
+   * @return false if the player entity is in a mushroom fields biome and
+   * {@link carpetaddonsnotfound.CarpetAddonsNotFoundSettings#disablePhantomSpawningInMushroomFields} rule is enabled,
+   * otherwise it is passed to next handler which could return true or false.
+   */
+  @Override
+  public boolean canSpawnPhantom(PlayerEntity playerEntity, ServerWorld world) {
+    BlockPos pos = playerEntity.getBlockPos();
+    RegistryEntry<Biome> registryEntry = world.getBiome(pos);
+    RegistryKey<Biome> biomePlayerIsIn = registryEntry.getKey().orElse(null);
+    if (CarpetAddonsNotFoundSettings.disablePhantomSpawningInMushroomFields &&
+        biomePlayerIsIn == BiomeKeys.MUSHROOM_FIELDS) {
+      return false;
+    }
+
+    return this.nextHandler.canSpawnPhantom(playerEntity, world);
+  }
+}
