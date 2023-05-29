54b16e91c47f097bce54916f6a77f64dbff64a6f
Gilly7CE <gilly7ce@gmail.com>
2023-05-29 10:58:44 +0100

    Refactor PhantomSpawnerMixin to leverage the phantomspawning package
    PhantomSpawnerMixin now has 1 mixin which targets the
    ServerWorld.getPlayers method invocation in the PhantomSpawner.spawn
    method. This will return a list of players which can have phantoms spawn
    around them as determined by the mod rules.

--- src/main/java/carpetaddonsnotfound/mixins/PhantomSpawnerMixin.java
+++ src/main/java/carpetaddonsnotfound/mixins/PhantomSpawnerMixin.java
@@ -1,58 +1,39 @@
 package carpetaddonsnotfound.mixins;
 
-import carpetaddonsnotfound.CarpetAddonsNotFoundSettings;
-import net.minecraft.entity.Entity;
+import carpetaddonsnotfound.phantomspawning.PhantomSpawningHandler;
+import carpetaddonsnotfound.phantomspawning.PhantomSpawningHandlerHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.PhantomSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import static carpetaddonsnotfound.CarpetAddonsNotFoundSettings.disablePhantomSpawningForCreativePlayers;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(PhantomSpawner.class)
 public abstract class PhantomSpawnerMixin {
   /**
-   * Redirects the isSpectator method of the playerEntity when called in the 'phantomSpawner.spawn' method. This is
-   * called in a loop of all world player entities, and we only want to spawn phantoms around players who are not in
-   * spectator mode or creative. This allows us to apply additional checks instead of just whether the player is in
-   * spectator. I did not know any other way of implementing this additional conditional!
+   * Modifies the list of {@link PlayerEntity}'s which are looped through when attempting to spawn phantoms. The
+   * returned list will be the ones which this mod determines can have phantoms spawn around.
    *
    * @param instance
-   *         the player instance to check
+   *         the current {@link ServerWorld}
    *
-   * @return true if the player is in spectator mode or creative.
+   * @return the list of {@link PlayerEntity}'s which can have phantoms spawn around, according to the mod anyway!
    */
-  @Redirect(
-          method = "spawn",
-          at = @At(
-                  value = "INVOKE",
-                  target = "Lnet/minecraft/entity/player/PlayerEntity;isSpectator()Z"
-          )
-  )
-  private boolean isSpectatorOrCreative(PlayerEntity instance) {
-    boolean isSpectator = instance.isSpectator();
-    boolean isCreative = disablePhantomSpawningForCreativePlayers && instance.isCreative();
-    return isSpectator || isCreative;
-  }
-
-  @Redirect(
-          method = "spawn",
-          at = @At(
-                  value = "INVOKE",
-                  target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers" +
-                           "(Lnet/minecraft/entity/Entity;)V"
-          )
-  )
-  private void isInMushroomFieldsBiome(ServerWorld instance, Entity entity) {
-    BlockPos pos = entity.getBlockPos();
-    Biome biome = instance.getBiome(pos);
-    if (!CarpetAddonsNotFoundSettings.disablePhantomSpawningInMushroomFields ||
-        biome.getCategory() != Biome.Category.MUSHROOM) {
-      instance.spawnEntityAndPassengers(entity);
+  @Redirect(method = "spawn",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;getPlayers()Ljava/util/List;"))
+  private List<PlayerEntity> getPlayersThatPhantomsCanSpawnAround(ServerWorld instance) {
+    List<PlayerEntity> newList = new ArrayList<>();
+    for (PlayerEntity playerEntity : instance.getPlayers()) {
+      PhantomSpawningHandler phantomSpawningHandler = PhantomSpawningHandlerHelper.getPhantomSpawningHandler();
+      if (phantomSpawningHandler.canSpawnPhantom(playerEntity, instance)) {
+        newList.add(playerEntity);
+      }
     }
+
+    return newList;
   }
 }
