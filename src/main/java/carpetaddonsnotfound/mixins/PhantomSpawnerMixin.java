51d8e4ed1dcf0e7fabee9ecd048ff95a0ca3906d
Gilly7CE <gilly7ce@gmail.com>
2023-05-29 10:58:44 +0100

    Refactor PhantomSpawnerMixin to leverage the phantomspawning package
    PhantomSpawnerMixin now has 1 mixin which targets the
    ServerWorld.getPlayers method invocation in the PhantomSpawner.spawn
    method. This will return a list of players which can have phantoms spawn
    around them as determined by the mod rules.

--- src/main/java/carpetaddonsnotfound/mixins/PhantomSpawnerMixin.java
+++ src/main/java/carpetaddonsnotfound/mixins/PhantomSpawnerMixin.java
@@ -1,84 +1,39 @@
 package carpetaddonsnotfound.mixins;
 
-import carpetaddonsnotfound.CarpetAddonsNotFoundSettings;
-import carpetaddonsnotfound.helpers.ChunkManagerHelper;
-import carpetaddonsnotfound.mixins.invokers.SpawnHelperInfoInvokerMixin;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.SpawnGroup;
+import carpetaddonsnotfound.phantomspawning.PhantomSpawningHandler;
+import carpetaddonsnotfound.phantomspawning.PhantomSpawningHandlerHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.registry.RegistryEntry;
-import net.minecraft.world.SpawnHelper;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.spawner.PhantomSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import static carpetaddonsnotfound.CarpetAddonsNotFoundSettings.disablePhantomSpawningForCreativePlayers;
-import static carpetaddonsnotfound.CarpetAddonsNotFoundSettings.phantomsObeyHostileMobCap;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * This is originally adapted from the
- * <a href="https://github.com/Lunaar-SMP/lunaar-carpet-addons">lunaar carpet extensions mod</a>.
- * At time of writing this code, the lunaar mod is still in version 1.17 and 1.18 experimental snapshots and I really
- * wanted this functionality for 1.19+. If this mod will support versions below 1.19, then only 1.18 will include this
- * carpet rule out of respect for the lunaar mod.
- * <p>
- * Most of the code here is different to lunaar now, as the mob cap is per player and not global anymore.
- */
 @Mixin(PhantomSpawner.class)
 public abstract class PhantomSpawnerMixin {
   /**
-   * Redirects the isSpectator method of the playerEntity when called in the 'phantomSpawner.spawn' method. This is
-   * called in a loop of all world player entities, and we only want to spawn phantoms around players whose hostile mob
-   * cap is below the required values. This allows us to apply additional checks instead of just whether the player is
-   * in spectator. I did not know any other way of implementing this additional conditional!
+   * Modifies the list of {@link PlayerEntity}'s which are looped through when attempting to spawn phantoms. The
+   * returned list will be the ones which this mod determines can have phantoms spawn around.
    *
    * @param instance
-   *         the player instance to check
+   *         the current {@link ServerWorld}
    *
-   * @return true if the player is in spectator mode or the player hostile mob cap is reached.
+   * @return the list of {@link PlayerEntity}'s which can have phantoms spawn around, according to the mod anyway!
    */
-  @Redirect(
-          method = "spawn",
-          at = @At(
-                  value = "INVOKE",
-                  target = "Lnet/minecraft/entity/player/PlayerEntity;isSpectator()Z"
-          )
-  )
-  private boolean isSpectatorCreativeOrAtMobCap(PlayerEntity instance) {
-    boolean isSpectator = instance.isSpectator();
-    boolean isCreative = disablePhantomSpawningForCreativePlayers && instance.isCreative();
-    if (isSpectator || isCreative) {
-      return true;
+  @Redirect(method = "spawn",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;getPlayers()Ljava/util/List;"))
+  private List<PlayerEntity> getPlayersThatPhantomsCanSpawnAround(ServerWorld instance) {
+    List<PlayerEntity> newList = new ArrayList<>();
+    for (PlayerEntity playerEntity : instance.getPlayers()) {
+      PhantomSpawningHandler phantomSpawningHandler = PhantomSpawningHandlerHelper.getPhantomSpawningHandler();
+      if (phantomSpawningHandler.canSpawnPhantom(playerEntity, instance)) {
+        newList.add(playerEntity);
+      }
     }
-    if (!phantomsObeyHostileMobCap) {
-      return false;
-    }
-    ChunkPos playerChunkPos = instance.getChunkPos();
-    SpawnHelper.Info info = ChunkManagerHelper.getInfo();
-    boolean canSpawn = ((SpawnHelperInfoInvokerMixin) info).invokeIsBelowCap(SpawnGroup.MONSTER, playerChunkPos);
-    return !canSpawn;
-  }
 
-  @Redirect(
-          method = "spawn",
-          at = @At(
-                  value = "INVOKE",
-                  target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers" +
-                           "(Lnet/minecraft/entity/Entity;)V"
-          )
-  )
-  private void isInMushroomFieldsBiome(ServerWorld instance, Entity entity) {
-    BlockPos pos = entity.getBlockPos();
-    RegistryEntry<Biome> registryEntry = instance.getBiome(pos);
-    if (!CarpetAddonsNotFoundSettings.disablePhantomSpawningInMushroomFields ||
-        registryEntry.getKey().get() != BiomeKeys.MUSHROOM_FIELDS) {
-      instance.spawnNewEntityAndPassengers(entity);
-    }
+    return newList;
   }
 }
