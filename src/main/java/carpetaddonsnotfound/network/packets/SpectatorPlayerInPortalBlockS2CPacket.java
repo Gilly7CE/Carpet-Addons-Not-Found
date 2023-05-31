64940349a871a78f99669c7a8d0cbcda2111d04f
Gilly7CE <gilly7ce@gmail.com>
2023-05-31 14:18:11 +0100

    Add SpectatorPlayerInPortalBlockS2CPacket for syncing the server and client
    New packet that allows the server to sync with the client when a spectator
    player is in a nether portal block. Packet will be sent from the server
    to the client, hence the "S2C" part of the class name.

---
+++ src/main/java/carpetaddonsnotfound/network/packets/SpectatorPlayerInPortalBlockS2CPacket.java
@@ -0,0 +1,29 @@
+package carpetaddonsnotfound.network.packets;
+
+import carpetaddonsnotfound.fakes.ClientPlayPacketListenerInterface;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.util.math.BlockPos;
+
+public final class SpectatorPlayerInPortalBlockS2CPacket implements Packet<ClientPlayPacketListener> {
+  private final BlockPos portalBlockPos;
+
+  public SpectatorPlayerInPortalBlockS2CPacket(BlockPos portalBlockPos) {
+    this.portalBlockPos = portalBlockPos;
+  }
+
+  @Override
+  public void write(PacketByteBuf buf) {
+    buf.writeBlockPos(portalBlockPos);
+  }
+
+  @Override
+  public void apply(ClientPlayPacketListener clientPlayPacketListener) {
+    ((ClientPlayPacketListenerInterface) clientPlayPacketListener).onSpectatorPlayerInPortalBlock(this);
+  }
+
+  public BlockPos getPortalBlockPos() {
+    return this.portalBlockPos;
+  }
+}
