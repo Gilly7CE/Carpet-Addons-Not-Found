486b7a24354ef1bcc3897a9d4ab8187728c71f00
Gilly7CE <gilly7ce@gmail.com>
2023-06-04 18:04:33 +0100

    Add update-wiki action & use proper mod version in CarpetAddonsNotFoundServer

--- src/main/java/carpetaddonsnotfound/CarpetAddonsNotFoundServer.java
+++ src/main/java/carpetaddonsnotfound/CarpetAddonsNotFoundServer.java
@@ -6,10 +6,26 @@ import carpet.api.settings.SettingsManager;
 import carpet.utils.Translations;
 import carpetaddonsnotfound.ruleobservers.MovableBlockEntitiesRuleObserver;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.Version;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 
 import java.util.Map;
 
 public class CarpetAddonsNotFoundServer implements CarpetExtension, ModInitializer {
+  public static final String MOD_ID = "carpet-addons-not-found"; // Must match with "id" key in fabric.mod.json
+  public static final String MOD_NAME;
+  public static final Version MOD_VERSION;
+
+  static {
+    ModMetadata metadata = FabricLoader.getInstance()
+                                       .getModContainer(MOD_ID)
+                                       .orElseThrow(RuntimeException::new)
+                                       .getMetadata();
+    MOD_NAME = metadata.getName();
+    MOD_VERSION = metadata.getVersion();
+  }
+
   @Override
   public void onInitialize() {
     CarpetServer.manageExtension(new CarpetAddonsNotFoundServer());
@@ -23,7 +39,7 @@ public class CarpetAddonsNotFoundServer implements CarpetExtension, ModInitializ
 
   @Override
   public String version() {
-    return "carpet-addons-not-found";
+    return MOD_VERSION.getFriendlyString();
   }
 
   @Override
