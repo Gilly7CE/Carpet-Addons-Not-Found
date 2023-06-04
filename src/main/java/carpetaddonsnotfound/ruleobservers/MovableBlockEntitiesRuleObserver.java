744344018756de389ffd180369f17fce20afc0bf
Gilly7CE <gilly7ce@gmail.com>
2023-06-04 17:52:27 +0100

    Refactor validation of the movableSpawner rule
    Rule observer is now for `movableBlockEntites` and a validator is used
    for `movableSpawners`.

---
+++ src/main/java/carpetaddonsnotfound/ruleobservers/MovableBlockEntitiesRuleObserver.java
@@ -0,0 +1,29 @@
+package carpetaddonsnotfound.ruleobservers;
+
+import carpet.api.settings.CarpetRule;
+import carpet.api.settings.SettingsManager;
+import carpetaddonsnotfound.CarpetAddonsNotFoundSettings;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import java.util.Objects;
+
+public final class MovableBlockEntitiesRuleObserver implements SettingsManager.RuleObserver {
+  @Override
+  public void ruleChanged(ServerCommandSource source, CarpetRule<?> changedRule, String userInput) {
+    // Using a string here directly feels wrong but likelihood of renaming this rule is low
+    if (!Objects.equals(changedRule.name(), "movableBlockEntities")) {
+      return;
+    }
+
+    boolean ruleEnabled = (boolean) changedRule.value();
+    if (ruleEnabled || !CarpetAddonsNotFoundSettings.movableSpawners) {
+      return;
+    }
+
+    source.sendFeedback(
+            Text.of("Warning: disabling `movableSpawners` as it requires `movableBlockEntities` to be enabled"),
+            true);
+    CarpetAddonsNotFoundSettings.movableSpawners = false;
+  }
+}
