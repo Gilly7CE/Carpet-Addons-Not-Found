07db403d1e2bb2b07f5a545e9e904dd024e8a644
Connor Slade <connor@connorcode.com>
2023-07-01 12:33:20 -0400

    Add passiveEndermen rule

---
+++ src/main/java/carpetaddonsnotfound/mixins/EndermanEntity_PassiveEndermen.java
@@ -0,0 +1,20 @@
+package carpetaddonsnotfound.mixins;
+
+import carpetaddonsnotfound.CarpetAddonsNotFoundSettings;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EndermanEntity.class)
+public class EndermanEntity_PassiveEndermen {
+  @Inject(method = "setTarget", at = @At("HEAD"), cancellable = true)
+  private void setTarget(final LivingEntity target, final CallbackInfo ci) {
+    if (CarpetAddonsNotFoundSettings.passiveEndermen && target instanceof PlayerEntity) {
+      ci.cancel();
+    }
+  }
+}
