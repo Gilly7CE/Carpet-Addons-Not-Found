bce153a5a9b58526fc208a702fce1303409f619d
Gilly7CE <gilly7ce@gmail.com>
2023-08-06 10:36:30 +0100

    Remove unused imports from EndFrameBlock_EyeToggleMixin

--- src/main/java/carpetaddonsnotfound/mixins/EndFrameBlock_EyeToggleMixin.java
+++ src/main/java/carpetaddonsnotfound/mixins/EndFrameBlock_EyeToggleMixin.java
@@ -17,9 +17,6 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-
-import static net.minecraft.block.Block.dropStack;
 
 @Mixin(EndPortalFrameBlock.class)
 public abstract class EndFrameBlock_EyeToggleMixin extends Block {
