5e4814301614a29ce61767a47e70aff9290183e7
Gilly7CE <gilly7ce@gmail.com>
2023-09-22 17:17:32 +0100

    Rework networking code following 1.20.2 update
    Lots of redundant code removed now. New Minecraft code makes things
    somewhat easier when regarding custom payloads.

--- src/main/java/carpetaddonsnotfound/network/ClientNetworkHandler.java
+++ src/main/java/carpetaddonsnotfound/network/ClientNetworkHandler.java
@@ -1,12 +1,9 @@
 package carpetaddonsnotfound.network;
 
 import carpetaddonsnotfound.CarpetAddonsNotFoundSettings;
-import io.netty.buffer.Unpooled;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.HashMap;
@@ -34,64 +31,12 @@ public class ClientNetworkHandler {
   /**
    * Taken from carpet base; handles incoming data, and it is executed on the main Minecraft Thread
    *
-   * @param data
-   *         the packet data
+   * @param compound
+   *         the nbt data
    * @param player
    *         the client side player
    */
-  public static void handleData(PacketByteBuf data, ClientPlayerEntity player) {
-    if (data == null) {
-      return;
-    }
-
-    int id = data.readVarInt();
-
-    if (id == CarpetAddonsNotFoundClient.HI) {
-      onHi(data);
-      return;
-    }
-
-    if (id == CarpetAddonsNotFoundClient.DATA) {
-      onSyncData(data, player);
-    }
-  }
-
-  /**
-   * Taken and tweaked from carpet base; executed during the "handshake" between client and server.
-   *
-   * @param data
-   *         packet data
-   */
-  private static void onHi(PacketByteBuf data) {
-    CarpetAddonsNotFoundClient.setCarpet();
-    // We can ensure that this packet is
-    // processed AFTER the player has joined
-    respondHello();
-  }
-
-  /**
-   * Taken and tweaked from carpet base.
-   */
-  private static void respondHello() {
-    CarpetAddonsNotFoundClient.getPlayer().networkHandler.sendPacket(new CustomPayloadC2SPacket(
-            CarpetAddonsNotFoundClient.CARPET_ADDONS_NOT_FOUND_CHANNEL,
-            (new PacketByteBuf(Unpooled.buffer())).writeVarInt(CarpetAddonsNotFoundClient.HELLO)));
-  }
-
-  /**
-   * Taken and tweaked from carpet base; syncs data between server and client players
-   *
-   * @param data
-   *         the packet data
-   * @param player
-   *         the client side player
-   */
-  private static void onSyncData(PacketByteBuf data, ClientPlayerEntity player) {
-    NbtCompound compound = data.readNbt();
-    if (compound == null) {
-      return;
-    }
-
+  public static void handleData(NbtCompound compound, ClientPlayerEntity player) {
     for (String key : compound.getKeys()) {
       if (!dataHandlers.containsKey(key)) {
         CarpetAddonsNotFoundSettings.LOG.error("Unknown carpet addons not found data: " + key);
