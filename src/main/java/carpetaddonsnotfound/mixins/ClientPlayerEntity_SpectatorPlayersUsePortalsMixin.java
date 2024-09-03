b076fc07e78031cc41e39b966bc745b7784d4095
Gilly7CE <gilly7ce@gmail.com>
2024-09-03 21:17:47 +0100

    Replace spectator portal rule networking code with client side player mixin
    Fixes broken code due to a 1.20.5 change with networking. The networking
    code was too complicated for a single use case and the client and server
    players would always execute the same movement code regardless so the
    rule could be implemented this way perfectly fine.

---
+++ src/main/java/carpetaddonsnotfound/mixins/ClientPlayerEntity_SpectatorPlayersUsePortalsMixin.java
@@ -0,0 +1,29 @@
+package carpetaddonsnotfound.mixins;
+
+import carpetaddonsnotfound.mixins.invokers.EntityInvokerMixin;
+import carpetaddonsnotfound.spectatorplayersuseportals.SpectatorPlayersUsePortalsRule;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.MovementType;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class ClientPlayerEntity_SpectatorPlayersUsePortalsMixin extends AbstractClientPlayerEntity implements
+                                                                                                            EntityInvokerMixin {
+
+  public ClientPlayerEntity_SpectatorPlayersUsePortalsMixin(ClientWorld world,
+                                                            GameProfile profile) {
+    super(world, profile);
+  }
+
+  @Inject(method = "move", at = @At("TAIL"))
+  private void movePlayerInSpectator(MovementType movementType, Vec3d movement, CallbackInfo ci) {
+    SpectatorPlayersUsePortalsRule.movePlayerInSpectator(this);
+  }
+}
